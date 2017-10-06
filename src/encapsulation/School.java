785932de9295ba6b062118a93d9ca49239ed17ae
taous86 <taousou@gmail.com>
2017-10-06 11:10:25 -0400

    test

---
+++ src/encapsulation/School.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class School {
+	public static void main(String[] args) {
+	Student student1=new Student();
+	student1.course="Java";
+	student1.setname("taous");
+	System.out.println(student1.getName());
+	student1.setSnn(1234567);
+	int social=student1.getSnn();
+	System.out.println(social);
+}
+}
