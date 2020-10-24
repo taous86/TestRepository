b23e64bba044275882a6c7ea8699f91c04a11f6b
taoussoukacha <63362628+taoussoukacha@users.noreply.github.com>
2020-10-24 10:28:24 -0400

    added Student.java clean up for School

--- src/encapsulation/School.java
+++ src/encapsulation/School.java
@@ -4,10 +4,10 @@ public class School {
 	public static void main(String[] args) {
 	Student student1=new Student();
 	student1.course="Java";
-	student1.setname("taous");
+	student1.setName("taous");
 	System.out.println(student1.getName());
 	student1.setSnn(1234567);
 	int social=student1.getSnn();
-	System.out.println(social);
+
 }
 }
---
+++ src/encapsulation/Student.java
@@ -0,0 +1,26 @@
+package encapsulation;
+
+public class Student {
+    public String course;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Name;
+
+    public int getSnn() {
+        return Snn;
+    }
+
+    public void setSnn(int snn) {
+        Snn = snn;
+    }
+
+    private int Snn;
+
+}
